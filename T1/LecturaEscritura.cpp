3864b2ead7721f8b54941f8c2b67744312444dbd
PatriciaFraile <patricia.fraile@cesjuanpablosegundo.es>
2023-01-04 23:42:05 +0100

    subir

---
+++ T1/LecturaEscritura.cpp
@@ -0,0 +1,16 @@
+//Lectura o entrada de datos
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int numero;
+	cout<<"Escribe un numero";
+	cin>>numero;
+	
+	cout<<"\nEl numero es:"<<numero;
+	
+	return 0;
+
+}
